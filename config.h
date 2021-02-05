760a1ecedfe0971f71676cc9acc84d0936df94e7
arthur_potapchuk <aptyp.potatoch@gmail.com>
2021-02-05 21:30:11 +0300

    new input added

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define RDP_CONFIG_H
 
 #define DIR "/home/arthur/Learning/7sem/net/coursework/rdp/"
-#define SENT_FILE DIR "in.txt"
-#define RECEIVED_FILE DIR "out.txt"
+#define SENT_FILE DIR "in.pdf"
+#define RECEIVED_FILE DIR "out.pdf"
 #define HOST_NAME "34.71.125.234"
 #define PORT    5000
 
