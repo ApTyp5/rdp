0d741c84ec83926da242a811e4239130fb6237fa
arthur_potapchuk <aptyp.potatoch@gmail.com>
2021-02-15 15:42:31 +0300

    new input added

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 #define RDP_CONFIG_H
 
 #define DIR "/home/arthur/Learning/7sem/net/coursework/rdp/"
-#define SENT_FILE DIR "in.pdf"
-#define RECEIVED_FILE DIR "out.pdf"
-#define HOST_NAME "34.71.125.234"
+#define SENT_FILE DIR "in.xlsx"
+#define RECEIVED_FILE DIR "out.xlsx"
+#define HOST_NAME "localhost"
 #define PORT    5000
 #define TCP_PORT        6023
 
--- debug.h
+++ debug.h
@@ -4,6 +4,6 @@
 
 #include <stdio.h>
 #pragma GCC diagnostic ignored "-Wformat-zero-length"
-#define D(tag, message, ...)         //printf("%s:\t\t%s\n", tag, message); printf(""__VA_ARGS__)
+#define D(tag, message, ...)         printf("%s:\t\t%s\n", tag, message); printf(""__VA_ARGS__)
 
 #endif //RDP_DEBUG_H
